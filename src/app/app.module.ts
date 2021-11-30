823b28dbf8a54dea5f373b25f1f944444178f36e
Ankit Rathore <Ankit.Rathore@ivanti.com>
2021-11-30 16:10:47 +0530

    Adding graphql

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { ListComponent } from './components/list/list.component';
 import { AgGridModule } from 'ag-grid-angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import {HttpLink} from 'apollo-angular/http';
+import {InMemoryCache} from '@apollo/client/core';
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,9 +23,21 @@ import { FormsModule } from '@angular/forms';
     FormsModule,
     AgGridModule.withComponents([]),
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
   ],
-  providers: [],
+  providers: [{
+    provide: APOLLO_OPTIONS,
+    useFactory: (httpLink: HttpLink) => {
+      return {
+        cache: new InMemoryCache(),
+        link: httpLink.create({
+          uri: 'https://48p1r2roz4.sse.codesandbox.io',
+        }),
+      };
+    },
+    deps: [HttpLink],
+  },],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
